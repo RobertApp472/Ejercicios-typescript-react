16ce44332cd398072a8786964ff6b53177b9ece3
Robert <robetdlc@gmail.com>
2026-02-09 23:32:58 -0500

    Corregir base para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Ejercicios-typescript-react/',
   plugins: [react()],
 })
